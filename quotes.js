3210139096114399f83a6606c9b7478f6b256193
Yousif Al-hamadani <yousifal-hamadani@Yousifs-MBP.home>
2022-09-18 22:47:03 -0500

    Quote Generator

---
+++ quotes.js
@@ -0,0 +1,104 @@
+const localQuotes = [
+    {
+        "text": "The bravest heart is the one that stays close to Allah (God), even, when it’s in pain.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "When you forget that you need Allah (God), He puts you in a situation that causes you to call upon Him. And that’s for your own good.",
+        "author": "Omar Suleiman",
+        "tag": "general"
+    },
+    {
+        "text": "Every man dies. Not every man really lives.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Never underestimate the power of Dua (supplication).",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "From small beginnings come great things.",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "Worldly life is short, so turn to Allah (God) before you return to Allah (God).",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "To Allah (God) is your return, all of you, and He will inform you of what you used to do.",
+        "author": "Quran 5:105",
+        "tag": "general"
+    },
+    {
+        "text": "Whoever fears Allah (God), Allah (God) will find a way out for him (from every difficulty) and He will provide for him from sources that he could never have imagined.",
+        "author": "Quran 65:2-3",
+        "tag": "general"
+    },
+    {
+        "text": "The reality of manners is that it results from beautiful character. Thus, manners is the manifestations of the integrity and strength in one’s inward personality into action.",
+        "author": "Ibn Rajab",
+        "tag": "general"
+    },
+    
+    {
+        "text": "Allah (God) does not burden a soul beyond that it can bear.",
+        "author": "Quran 2:286",
+        "tag": "general"
+    },
+    {
+        "text": "And whoever turns away from My remembrance – indeed, he will have a depressed life",
+        "author": "Quran 20:124",
+        "tag": "general"
+    },
+    {
+        "text": "Taking pains to remove the pains of others is the true essence of generosity.",
+        "author": "Abu Bakr (R.A)",
+        "tag": ""
+    },
+    {
+        "text": "They plan, and Allah (God) plans. Surely, Allah (God) is the best of planners.",
+        "author": "Quran 8:30",
+        "tag": "general"
+    },
+    {
+        "text": "Allah (God) does not intend to make difficulty for you, but He intends to purify you and complete His favor upon that you may be grateful.",
+        "author": " Quran 5:6",
+        "tag": "general"
+    },
+    {
+        "text": "And it is He who created the night and day, and the sun and moon.",
+        "author": "Quran 21:33",
+        "tag": "general"
+    },
+    {
+        "text": "The best among you are those who have the best manners and best character.",
+        "author": "Sahih Bukhari 6029",
+        "tag": "general"
+    },
+    //
+    {
+        "text": "For indeed, with hardship [will be] ease.",
+        "author": "Quran 94:5",
+        "tag": "general"
+    },
+    {
+        "text": "No one can bring you true happiness except Allah (God).",
+        "author": "Anonymous",
+        "tag": "general"
+    },
+    {
+        "text": "And whoever puts all his trust in Allah (God), then He will suffice him.",
+        "author": "Quran 65:3 ",
+        "tag": "general"
+    },
+    {
+        "text": "The richest of the rich is the one who is not a prisoner to greed.",
+        "author": "Ali Ibn Abi Talib (R.A)",
+        "tag": "general"
+    },
+]
--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log('testing:');
+const quoteContainer = document.getElementById('qoute-container')
+const quoteText = document.getElementById('qoute')
+const quoteAurhor = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('New-Quote')
+
+// show new quote
+
+function newQuote () {
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    quoteAurhor.textContent = quote.author;
+    quoteText.textContent = quote.text;
+
+
+
+
+
+if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote')
+
+} else {
+    quoteText.classList.remove('long-quote')
+}
+quoteText.textContent = quote.text;
+
+
+
+
+// tweet
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAurhor.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+
+
+
+
+//event listner
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+}
+
+newQuote();
